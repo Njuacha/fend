5a765ac458f339eeef916788bb131bfe4aac21a4
Njuacha <betomis2006@yahoo.com>
2020-06-24 22:21:06 +0200

    feat: create and add navigation menu items dynamically after page has loaded.

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -1,28 +1,28 @@
 /**
- * 
+ *
  * Manipulating the DOM exercise.
  * Exercise programmatically builds navigation,
  * scrolls to anchors from navigation,
  * and highlights section in viewport upon scrolling.
- * 
+ *
  * Dependencies: None
- * 
+ *
  * JS Version: ES2015/ES6
- * 
+ *
  * JS Standard: ESlint
- * 
+ *
 */
 
 /**
  * Define Global Variables
- * 
+ *
 */
 
 
 /**
  * End Global Variables
  * Start Helper Functions
- * 
+ *
 */
 
 
@@ -30,10 +30,27 @@
 /**
  * End Helper Functions
  * Begin Main Functions
- * 
+ *
 */
 
 // build the nav
+const sections = document.querySelectorAll('[data-nav]');
+const navMenuFragment = document.createDocumentFragment();
+
+for (const section of sections) {
+  const id = section.getAttribute('id');
+  const sectionName = section.getAttribute('data-nav');
+  // create list item an append to navMenuFragment
+  const menuItem = document.createElement('li');
+  menuItem.setAttribute('data-section',id);
+  menuItem.innerText = sectionName;
+  navMenuFragment.appendChild(menuItem);
+}
+
+const navBarList = document.querySelector('#navbar__list');
+navBarList.appendChild(navMenuFragment);
+
+
 
 
 // Add class 'active' to section when near top of viewport
@@ -45,13 +62,11 @@
 /**
  * End Main Functions
  * Begin Events
- * 
+ *
 */
 
-// Build menu 
+// Build menu
 
 // Scroll to section on link click
 
 // Set sections as active
-
-
