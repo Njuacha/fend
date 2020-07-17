1a9e01b292f0e8c9c78efa3f3dafdba955e4c44e
Njuacha <betomis2006@yahoo.com>
2020-07-17 23:01:17 +0200

    refactor: try to rescue css styling a little bit.

--- projects/weather-journal-app/website/app.js
+++ projects/weather-journal-app/website/app.js
@@ -1,12 +1,44 @@
 /* Global Variables */
 const apiKey = '&appid='+'5663eeffe68b29c9ec1cbb2127466ba5';
 const baseUrl = 'https://api.openweathermap.org/data/2.5/weather?zip=';
-// api.openweathermap.org/data/2.5/weather?zip={zip code},{country code}&appid={your api key}
 
 // Create a new date instance dynamically with JS
 const d = new Date();
 const date = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
 
+// Add eventlistener for handling click event on Gererate button
+document.querySelector('#generate').addEventListener('click', () => {
+   const zipCode = document.querySelector('#zip').value;
+   const feelings = document.querySelector('#feelings').value;
+
+   // get current weather from openWeathermap api
+   getCurrentTemperature(zipCode)
+   // then post the temperature of city of zip code together with feelings of user typed in and the current date
+   .then(function(temperature) {
+     postData('http://localhost:3000/add', {temp: temperature, date: date, user_response: feelings});
+   })
+   .then(() =>
+     updateUI()
+   )
+
+});
+
+// asynchronous function to fetch the current temperature of a city based on its zip code from the openWeahterMap api
+const getCurrentTemperature = async (zipCode) => {
+
+    const res = await fetch(baseUrl+zipCode+apiKey);
+
+    try {
+      const data = await res.json();
+      const temperature = data.main.temp;
+      return temperature;
+    } catch(error) {
+        console.log("error", error);
+    }
+
+}
+
+// asynchronous function to post the data to the server
 const postData = async ( url = '', data = {}) => {
 
     const response = await fetch(url, {
@@ -26,42 +58,7 @@ const postData = async ( url = '', data = {}) => {
     }
 }
 
-
-
-// Add eventlistener for handling click event on Gererate button
-document.querySelector('#generate').addEventListener('click', () => {
-   const zipCode = document.querySelector('#zip').value;
-   const feelings = document.querySelector('#feelings').value;
-
-   // get current weather from openWeathermap api
-   getCurrentTemperature(zipCode)
-   // then post the temperature of city of zip code together with feelings of user typed in and the current date
-   .then(function(temperature) {
-     postData('http://localhost:3000/add', {temp: temperature, date: date, user_response: feelings});
-   })
-   .then(() =>
-     updateUI()
-   )
-
-});
-
-// function to fetch the current temperature of a city based on its zip code from the openWeahterMap api
-const getCurrentTemperature = async (zipCode) => {
-
-    const res = await fetch(baseUrl+zipCode+apiKey);
-
-    try {
-      const data = await res.json();
-      const temperature = data.main.temp;
-      return temperature;
-    } catch(error) {
-        console.log("error", error);
-    }
-
-}
-
-
-
+// asynchronous function to get recent data from server and update the ui
 const updateUI = async () => {
   const res = await fetch('http://localhost:3000/all');
 
