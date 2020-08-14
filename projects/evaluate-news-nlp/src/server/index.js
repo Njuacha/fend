9ba93c83b1ebd542847131fd12749d23aff273f7
Njuacha <betomis2006@yahoo.com>
2020-08-14 17:16:16 +0200

    test api call in server hide api api key

--- projects/evaluate-news-nlp/src/server/index.js
+++ projects/evaluate-news-nlp/src/server/index.js
@@ -1,23 +1,65 @@
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+const dotenv = require('dotenv');
+dotenv.config();
 
-const app = express()
+// var textapi = new meaningcloud({
+//   application_key: process.env.API_KEY
+// });
 
-app.use(express.static('dist'))
+//
+// var path = require('path')
+// const express = require('express')
+// const mockAPIResponse = require('./mockAPI.js')
+//
+// const app = express()
+//
+// app.use(express.static('dist'))
+//
+// console.log(__dirname)
+//
+// app.get('/', function (req, res) {
+//     //res.sendFile('dist/index.html')
+//     res.sendFile(path.resolve('src/client/views/index.html'))
+// })
+//
+// // designates what port the app will listen to for incoming requests
+// app.listen(8080, function () {
+//     console.log('Example app listening on port 3030!')
+// })
+//
+// app.get('/test', function (req, res) {
+//     res.send(mockAPIResponse)
+// })
 
-console.log(__dirname)
+var https = require('follow-redirects').https;
+var fs = require('fs');
+var apiKey = process.env.API_KEY;
 
-app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
-    //res.sendFile(path.resolve('src/client/views/index.html'))
-})
+var text = escape("This is a very nice restaurant");
+console.log('/sentiment-2.1?key='+apiKey+'&lang=en&txt='+text+'&model=Restaurants');
+var options = {
+  'method': 'POST',
+  'hostname': 'api.meaningcloud.com',
+  'path': '/sentiment-2.1?key='+apiKey+'&lang=en&txt='+text,
+  'headers': {
+   },
+  'maxRedirects': 20
+};
 
-// designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
-})
+var req = https.request(options, function (res) {
+  var chunks = [];
 
-app.get('/test', function (req, res) {
-    res.send(mockAPIResponse)
-})
+  res.on("data", function (chunk) {
+    chunks.push(chunk);
+  });
+
+  res.on("end", function (chunk) {
+    var body = Buffer.concat(chunks);
+    console.log(body.toString());
+  });
+
+  res.on("error", function (error) {
+    console.error(error);
+  });
+});
+
+req.end();
