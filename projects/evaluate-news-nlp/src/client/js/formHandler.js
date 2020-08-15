4daeaf16c3ba20901f9326e6742b4ba642f640f9
Njuacha <betomis2006@yahoo.com>
2020-08-15 14:13:42 +0200

    feat: display the result of sentiment analysis of text entered.

--- projects/evaluate-news-nlp/src/client/js/formHandler.js
+++ projects/evaluate-news-nlp/src/client/js/formHandler.js
@@ -1,18 +1,21 @@
 function handleSubmit(event) {
     event.preventDefault()
 
-    // check what text was put into the form field
-    // let formText = document.getElementById('name').value
-    // Client.checkForName(formText)
-
+    // very simple input validation
+    let formText = document.getElementById('formText').value;
+    if(formText == "") {
+      return;
+    }
+    // remove white spaces
+    formText = formText.trim();
+    // add escape characters so that it would be compactible with format required by sentiment api
+    formText = escape(formText);
     console.log("::: Form Submitted :::")
-    const text = escape('Very wonderful app.')
-    postData('http://localhost:8081/getResults', {text: text})
-    .then(function(newData) {
-        console.log('Subjective: '+newData.subjectivity);
-        console.log('Irony: '+newData.irony);
-        //console.log('')
-    })
+
+    postData('http://localhost:8081/getResults', {text: formText})
+    .then((newData)  =>
+      updateUI(newData)
+    )
 }
 
 const postData = async (url = '', data = {}) => {
@@ -32,6 +35,27 @@ const postData = async (url = '', data = {}) => {
   }catch(error) {
     console.log('error', error);
   }
+}
+
+const updateUI = (newData) => {
+     const objectiveOrSubjective = newData.subjectivity;
+     const irony = newData.irony;
+     const sentencesJsonArray = newData.sentence_list;
+     console.log('objectivity:'+objectiveOrSubjective);
+     console.log('irony:'+irony);
+     document.querySelector('#objectivity').innerText = objectiveOrSubjective;
+     document.querySelector('#irony').innerText = irony;
+
+     let tableContentFragment = document.createDocumentFragment();
+
+     for(const sentenceJson of sentencesJsonArray) {
+        let tableRow = document.createElement('tr');
+        tableRow.innerHTML = `<td class="result-value">${sentenceJson.text}</td> <td class="result-value">${sentenceJson.confidence}</td> <td class="result-value">${sentenceJson.score_tag}</td>`;
+        tableContentFragment.appendChild(tableRow);
+     }
+
+     document.querySelector('#table').appendChild(tableContentFragment);
+
 }
 
 export { handleSubmit }
